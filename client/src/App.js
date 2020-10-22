d78961ca5874f19c4f0ca325d9856760415688cd
shadow104 <josh.fusillo@gmail.com>
2020-10-22 12:06:30 -0400

    got the redirect to work with the data - thanks Jessicagit add -A

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,15 @@ import axios from 'axios';
 import Youtube from './components/VideoPlayer/Youtube'
 import './App.css';
 import React, { Component } from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import {BrowserRouter, Switch, Route, useHistory} from 'react-router-dom';
 
 export default class App extends Component {
   state = {
     searchName: '',
     newsResults: [],
     videos: [],
-    selectedVideo: null
+    selectedVideo: null,
+    redirect: false
 }
 
 handleSearchName = (event) => {
@@ -47,18 +47,41 @@ getVideo = async () => {
     })
 }
 
+getArtist = () =>{
+  axios
+    .get(`https://rest.bandsintown.com/artists/${this.state.searchName}/?app_id=2bfefdd4b6571ebbc6ba9afbb5bc55d8`)
+    .then(response => console.log(response.data))
+}
+
+getEvent = () =>{
+  axios
+    .get(`https://rest.bandsintown.com/artists/${this.state.searchName}/events/?app_id=2bfefdd4b6571ebbc6ba9afbb5bc55d8`)
+    .then(response => console.log(response.data))
+}
+
  handleVideoSelect = (video) => {
         this.setState({selectedVideo: video})
-    }  
+  }  
+
+
 
 handleSubmit = (event) => {
   event.preventDefault();
   this.getNews();
   this.getVideo();
+  this.getArtist();
+  this.getEvent();
+  this.setState({
+    redirect: true
+  })
+  
+  
       
 }
   
   render() {
+   
+
     return (
       <div className="App">        
         <BrowserRouter>              
@@ -67,14 +90,11 @@ handleSubmit = (event) => {
               value={this.state.searchName} 
               onChange={this.handleSearchName}
               handleSubmit={this.handleSubmit}
+              redirect={this.state.redirect}
             />
           )}/>
-          <Route path="/MainTwo" render={(props) => <MainTwo {...props}     
-              newsResults={this.state.newsResults}
-            />}/>
-            {/* <Route exact path="/" component = {Main} />
-            <Route exact path="/MainTwo" component = {MainTwo} /> */}
-
+          <Route path="/MainTwo" component = {MainTwo} />
+          
         </Switch>
          </BrowserRouter>          
        <div className="footer__main">
