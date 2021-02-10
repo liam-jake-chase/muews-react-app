52025d05e74bfb521f2e7c402c55b4e7ff3cc30f
Josh Fusillo <josh.fusillo@gmail.com>
2021-02-10 11:29:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/josh-fusillo-capstone-meuws/precache-manifest.eb6e940c4e5bad0fc010f9cf628c2e4a.js"
+  "/josh-fusillo-capstone-muews/precache-manifest.76ca34bd4a7f91e7077ef9d7fcd4012e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/josh-fusillo-capstone-meuws/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/josh-fusillo-capstone-muews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
